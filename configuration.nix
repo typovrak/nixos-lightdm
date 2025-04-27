0b04a56564ea375058c42f2305f579e1eb546c62
typovrak <typovrak@gmail.com>
2025-04-27 23:55:15 +0200

    revert

--- configuration.nix
+++ configuration.nix
@@ -36,14 +36,9 @@ in {
 
 	services = {
 		displayManager.defaultSession = "none+i3";
-		xserver.displayManager = {
-			setupCommands = ''
-				xrandr --output eDP-1-1 --primary --auto --output HDMI-0 --off
-			'';
-			lightdm = {
-				enable = true;
-				greeters.gtk.enable = true;
-			};
+		xserver.displayManager.lightdm = {
+			enable = true;
+			greeters.gtk.enable = true;
 		};
 	};
 }
